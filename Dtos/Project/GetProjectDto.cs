3ba9ff58878cdd1d2f6431bdfb457a6034908ccb
Patryk-S-W <10290008+Patryk-S-W@users.noreply.github.com>
2023-08-01 03:51:14 +0200

    fix: empty string instead null & add DateTime Type

--- Dtos/Project/GetProjectDto.cs
+++ Dtos/Project/GetProjectDto.cs
@@ -2,15 +2,19 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace tech_blog_backend.Dtos.Project
 {
     public class GetProjectDto
     {
         public int Id { get; set; }
-        public string Title { get; set; }
-        public string Image { get; set; }
-        public string Date { get; set; }
-        public string Text { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Image { get; set; } = string.Empty;
+        [DataType(DataType.Date)]
+        public DateTime CreatedDate { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime UpdatedDate { get; set; }
+        public string Text { get; set; } = string.Empty;
     }
 }
--- Models/Announcement.cs
+++ Models/Announcement.cs
@@ -2,18 +2,22 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace tech_blog_backend.Models
 {
     public class Announcement
     {
         public int Id { get; set; }
-        public string Title { get; set; }
-        public string Image { get; set; }
-        public string Date { get; set; }
-        public string Text { get; set; }
-        public string Category { get; set; }
-        public string Duration { get; set; }
-        public string Button { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Image { get; set; } = string.Empty;
+        [DataType(DataType.Date)]
+        public DateTime CreatedDate { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime UpdatedDate { get; set; }
+        public string Text { get; set; } = string.Empty;
+        public string Category { get; set; } = string.Empty;
+        public string Duration { get; set; } = string.Empty;
+        public string Button { get; set; } = string.Empty;
     }
 }
